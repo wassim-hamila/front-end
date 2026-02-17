543bbf711d4df2015743105d47ce478775b9f6aa
wassim-636 <bg.wassimhkm@gmail.com>
2026-02-17 16:31:33 +0100

    Fix useEffect dependencies

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+console.log(import.meta.env.VITE_API_URL);
 
 const api = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
